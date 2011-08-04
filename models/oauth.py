fb7e93a5953b9e1c147ef1e5d14dbfb3e4f5c2ba
Sy Khader <sy@sayedkhader.com>
2011-08-04 16:33:12 -0400

    Using another library instead of dmjson

--- models/oauth.py
+++ models/oauth.py
@@ -12,7 +12,7 @@ from wsgiref.handlers import CGIHandler
 
 sys.path.insert(0, join_path(dirname(__file__), 'lib')) # extend sys.path
 
-from demjson import decode as decode_json
+from simplejson import loads as decode_json
 
 from google.appengine.api.urlfetch import fetch as urlfetch, GET, POST
 from google.appengine.ext import db
