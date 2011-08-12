8a18d8d91106f3bf1598113e1381916aa0726a2d
Barbara <z4beth@gmail.com>
2011-08-12 16:22:40 -0400

    Updating back to real constants.

--- util/consts.py
+++ util/consts.py
@@ -7,7 +7,7 @@ NAME = 'ReferredU'
 # Domain Stuff
 USING_DEV_SERVER    = True if 'Development' in os.environ.get('SERVER_SOFTWARE', "") else False
 PROTOCOL            = 'http' 
-APP_DOMAIN          = 'None' if USING_DEV_SERVER else 'sy-willet.appspot.com'
+APP_DOMAIN          = 'None' if USING_DEV_SERVER else 'social-referral.appspot.com'
 DOMAIN              = os.environ['HTTP_HOST'] if USING_DEV_SERVER else APP_DOMAIN 
 URL                 = urlunsplit((PROTOCOL, DOMAIN, '', '', '')) 
 
@@ -16,8 +16,8 @@ LANDING_CAMPAIGN_UUID = '28e530db44bf45e5'
 
 # Twitter Stuff
 TWITTER_SEARCH_URL = 'http://search.twitter.com/search.json?'
-TWITTER_KEY = 'AGjI5z0RZFX7pq3i7nJgtg'
-TWITTER_SECRET = 'aCUHHM1ZWcM4z35OORhbPuzIEsGlEB2QIl8Ysl3xn1o'
+TWITTER_KEY = '2O3uHYkLKlHdy2PECgP3Q'
+TWITTER_SECRET = 'W3fe6c1ZP3D4RyymqszxXfNcJzvu0fN82Nf3S68078'
 
 # Mixpanel Stuff
 MIXPANEL_API_KEY = 'a4bed9e726adf0a972fe2277784b6f51'
@@ -26,9 +26,8 @@ MIXPANEL_SECRET  = 'd1c0a8833b32d0922f6ef91704925b5f'
 MIXPANEL_TOKEN   = '5e7c1fdd252ecdccfecf1682df9f76a2'
 
 # Facebook Stuff
-FACEBOOK_APP_ID = '175144029215141'
-FACEBOOK_APP_SECRET = '49fb2c41881e4f5aafe16b3dffdd9c0b'
-FACEBOOK_QUERY_URL = 'https://graph.facebook.com/'
+FACEBOOK_APP_ID = '181838945216160'
+FACEBOOK_APP_SECRET = 'a34a3f5ba2d87975ae84dab0f2a47453'
 
 # Klout Stuff
 KLOUT_API_KEY = '6gs66hdaj6vmung5mtg3aeka'
