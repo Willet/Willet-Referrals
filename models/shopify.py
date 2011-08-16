2b35542abcba5d385d62cf19a364bbf7d756ced3
Barbara <z4beth@gmail.com>
2011-08-16 19:36:42 -0400

    Shopify api atuff.

--- models/shopify.py
+++ models/shopify.py
@@ -16,7 +16,7 @@ from models.model         import Model
 from util.helpers         import generate_uuid
 
 
-class Item:
+class ShopifyItem:
 
     def __init__( self, name, price, product_id ):
         self.name  = name
@@ -34,41 +34,47 @@ class Item:
 
 class ShopifyOrder(Model):
     """Model storing shopify_order data"""
-    uuid     = db.StringProperty( indexed = True )
+    order_id = db.StringProperty( indexed = True )
     created  = db.DateTimeProperty(auto_now_add=True)
-
-    store_name
-    store_url
-    order_number
-    subtotal_price # no taxes
-    referring_site # might be useful
-
-
-    items ..
+    store_name     = db.StringProperty( indexed = False )
+    store_url      = db.LinkProperty( indexed = False )
+    order_number   = db.StringProperty( indexed = False )
+    subtotal_price = db.FloatProperty( indexed = False ) # no taxes
+    referring_site = db.LinkProperty( indexed = False ) # might be useful
+    items          = db.ListProperty( ShopifyItem, default = None )
 
     user = db.ReferenceProperty( db.Model, default = None, collection_name="shopify_purchases" )
     campaign = db.ReferenceProperty( db.Model, collection_name="shopify_orders" )
 
     def __init__(self, *args, **kwargs):
-        self._memcache_key = kwargs['uuid'] if 'uuid' in kwargs else None 
+        self._memcache_key = kwargs['order_id'] if 'order_id' in kwargs else None 
         super(ShopifyOrder, self).__init__(*args, **kwargs)
-    
+
+    def add_item( self, item_name, item_price, item_id ):
+        i = ShopifyItem( name=item_name, price=item_price, product_id=item_id )
+        self.items.append( i )
+
+        self.put()
+
     @staticmethod
     def _get_from_datastore( uuid ):
         """Datastore retrieval using memcache_key"""
-        return db.Query(ShopifyOrder).filter('uuid =', uuid).get()
+        return db.Query(ShopifyOrder).filter('order_id =', uuid).get()
 
-def create_shopify_order( link, campaign, referree_uid, referree ):
-    uuid = generate_uuid(16)
+def create_shopify_order( campaign, order_id, order_num,
+                          subtotal, referrer, user ):
     
-    c = ShopifyOrder( key_name     = uuid,
-                    uuid         = uuid,
-                    link         = link,
-                    referrer     = link.user,
-                    referree     = referree,
-                    referree_uid = referree_uid,
-                    campaign     = campaign )
-    c.put()
+    o = ShopifyOrder( key_name   = order_id,
+                    order_id     = order_id,
+                    campaign     = campaign,
+                    store_name   = campaign.store_name,
+                    store_url    = campaign.store_url,
+                    order_number = order_num,
+                    subtotal_price = subtotal, 
+                    referring_site = referrer,
+                    user         = user )
+    o.put()
 
-    return c # return incase the caller wants it
+    return o # return incase the caller wants it
+
 
--- shopify.py
+++ shopify.py
@@ -11,9 +11,11 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-from util.consts import *
 
-def getShopifyOrder( campaign_uuid, order_id ):
+from models.shopify import *
+from util.consts    import *
+
+def GetShopifyOrder( campaign_uuid, order_id ):
     campaign = get_campaign_by_uuid( campaign_uuid )
 
     url = '%s/admin/orders/#%s.json' % ( campaign.target_url, order_id )
@@ -46,14 +48,72 @@ def getShopifyOrder( campaign_uuid, order_id ):
         # handle gracefully
         return # TODO(Barbara): fix this in the future
 
+    # Grab the data about the order from Shopify
     order = json.loads( result.content )['order'] # Fetch the order
+
+    items = []
+    order_id = order_num = referring_site = subtotal = None
+    bill_addr = None
+    for k, v in order.iteritems():
+
+        # Grab order details
+        if k == 'id':
+            order_id = v
+        elif k == 'subtotal_price':
+            subtotal = v
+        elif k == 'order_number':
+            order_num = v
+        elif k == 'referring_site':
+            referring_site = v
+    
+        # Grab the purchased items and save some information about them.
+        elif 'line_items' in k:
+            for j in v:
+                i = ShopifyItem( name=j['name'], price=j['price'], product_id=j['product_id'])
+                items.append( i )
+
+        # Store User/ Customer data
+        elif k == 'billing_address':
+            if user:
+                user.city      = v['city']
+                user.province  = v['province']
+                user.country_code = v['country_code']
+                user.latitude  = v['latitude']
+                user.longitude = v['longitude']
+            else:
+                bill_addr = v
+
+        elif k == 'customer':
+            if user is None:
+                user = get_or_create_user_by_email( v['email'] )
+
+            user.first_name = v['first_name']
+            user.last_name  = v['last_name']
+            user.shopify_customer_id = v['id']
+
+            if bill_addr:
+                user.city         = bill_addr['city']
+                user.province     = bill_addr['province']
+                user.country_code = bill_addr['country_code']
+                user.latitude     = bill_addr['latitude']
+                user.longitude    = bill_addr['longitude']
+    
+    # Save the new User data        
+    user.put()
+
+    # Make the ShopifyOrder
+    o = create_shopify_order( campaign, order_id, order_num, subtotal, referring_site, user )
+
+    # Store the purchased items in the order
+    o.items.extend( items )
+
 
 
 ##-----------------------------------------------------------------------------##
 ##------------------------- The URI Router ------------------------------------##
 ##-----------------------------------------------------------------------------##
 def main():
-    application = webapp.WSGIApplication([ ], debug=USING_DEV_SERVER)
+    application = webapp.WSGIApplication([ ('/shopifyPurchase', GetShopifyOrder) ], debug=USING_DEV_SERVER)
     run_wsgi_app(application)
 
 if __name__ == "__main__":
