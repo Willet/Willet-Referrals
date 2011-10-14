afe9ade5446f272e35b3d4ec9c26a5214c1ab6bd
Matthew Harris <harrismc@gmail.com>
2011-10-14 13:58:33 -0400

    now with stack traces from js

--- apps/admin/processes.py
+++ apps/admin/processes.py
@@ -38,13 +38,15 @@ class TrackRemoteError(webapp.RequestHandler):
     def get(self):
         referer = self.request.headers.get('referer')
         error = self.request.get('error')
+        stack_trace = self.request.get('st')
         mail.send_mail(
             sender = 'rf.rs error reporting <Barbara@rf.rs>',
             to = 'barbara@getwillet.com',
             subject = 'Javascript callback error',
-            body = 'We encountered an error on page %s:\n%s' % (
+            body = 'We encountered an error on page %s:\n%s\n%s' % (
                 referer,
-                error
+                error,
+                stack_trace
             )
         )
         self.redirect('%s/static/imgs/noimage.png' % URL)
