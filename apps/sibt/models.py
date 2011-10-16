88fdd21ea9853c80bf68b290a2e7278542b635b3
fraserharris <fraser.harris@gmail.com>
2011-10-15 22:43:26 -0400

    Even better email

--- apps/sibt/models.py
+++ apps/sibt/models.py
@@ -88,14 +88,29 @@ class SIBT(App):
         bingo( 'sibt_instance_started' )
 
         if not user.is_admin() and "social-referral" in URL:
-            Email.emailBarbara('SIBT INSTANCE: \n uuid=%s \n user.key=%s \n page=%s \n name=%s \n fb_uuid=%s \n fb_access_token=%s' % (
-                uuid,
-                user.key(), 
-                link.target_url,
-                user.get_full_name(),
-                user.get_attr('fb_identity'),
-                user.get_attr('fb_access_token')
-            )
+            try:
+                Email.emailBarbara("""
+                    SIBT INSTANCE:<br />
+                    uuid= %s<br />
+                    user.key= %s<br />
+                    page= %s<br />
+                    link= http://rf.rs/%s<br />
+                    name= %s<br />
+                    fb_uuid= %s<br />
+                    fb_access_token= %s"""
+                    % (
+                        uuid,
+                        user.key(), 
+                        link.target_url,
+                        link.willt_url_code,
+                        user.get_full_name(),
+                        user.get_attr('fb_identity'),
+                        user.get_attr('fb_access_token')
+                    )
+                )
+           except Exception, e:
+               Email.emailBarbara('SIBT INSTANCE: error printing data: %s' % str(e))
+           
         return instance
 
 # Accessors --------------------------------------------------------------------
