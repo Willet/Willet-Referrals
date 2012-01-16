d7c5bd6a9d40c718e6ea81a8ce7d6e93d4a93e02
Barbara <z4beth@gmail.com>
2012-01-16 11:13:18 -0500

    Try/catch around all order processing so we never return a 500 to Shopify

--- apps/order/shopify/processes.py
+++ apps/order/shopify/processes.py
@@ -69,117 +69,121 @@ class OrderWebhookNotification(URIHandler):
         return self.post()
 
     def post(self):
-        logging.info("HEADERS : %s %r" % (
-            self.request.headers,
-            self.request.headers
+        # Try/catch around whole thing so we never return a 500 error to Shopify.
+        try:
+            logging.info("HEADERS : %s %r" % (
+                self.request.headers,
+                self.request.headers
+                )
             )
-        )
 
-        store_url = "http://%s" % self.request.headers['X-Shopify-Shop-Domain']
-        logging.info("store: %s " % store_url)
-        client = ClientShopify.get_by_url( store_url ) 
+            store_url = "http://%s" % self.request.headers['X-Shopify-Shop-Domain']
+            logging.info("store: %s " % store_url)
+            client = ClientShopify.get_by_url( store_url ) 
 
-        # Grab the data about the order from Shopify
-        order = json.loads(self.request.body) 
+            # Grab the data about the order from Shopify
+            order = json.loads(self.request.body) 
 
-        items = []
-        order_id = token = order_num = referring_site = subtotal = None
-        first_name = last_name = address1 = address2 = city = None
-        province = country_code = postal_code = latitude = longitude = None
-        phone = email = shopify_id = ip = accepts_marketing = None
+            items = []
+            order_id = token = order_num = referring_site = subtotal = None
+            first_name = last_name = address1 = address2 = city = None
+            province = country_code = postal_code = latitude = longitude = None
+            phone = email = shopify_id = ip = accepts_marketing = None
 
-        for k, v in order.iteritems():
-            logging.info("K: %s V: %s" % (k, v))
+            for k, v in order.iteritems():
+                logging.info("K: %s V: %s" % (k, v))
+                
+                # Grab order details
+                if k == 'id':
+                    order_id = v
+                elif k == 'subtotal_price':
+                    subtotal = v
+                elif k == 'order_number':
+                    order_num = v
+                elif k == 'referring_site':
+                    referring_site = v
+                elif k == 'token':
+                    token = v
             
-            # Grab order details
-            if k == 'id':
-                order_id = v
-            elif k == 'subtotal_price':
-                subtotal = v
-            elif k == 'order_number':
-                order_num = v
-            elif k == 'referring_site':
-                referring_site = v
-            elif k == 'token':
-                token = v
-        
-            # Grab the purchased items and save some information about them.
-            elif k == 'line_items':
-                for j in v:
-                    product = ProductShopify.get_by_shopify_id(str(j['product_id']))
-                    if product:
-                        items.append(product.key())
+                # Grab the purchased items and save some information about them.
+                elif k == 'line_items':
+                    for j in v:
+                        product = ProductShopify.get_by_shopify_id(str(j['product_id']))
+                        if product:
+                            items.append(product.key())
 
-            # Store User/ Customer data
-            elif k == 'billing_address':
-                address1           = v['address1']
-                address2           = v['address2']
-                city               = v['city']
-                province           = v['province']
-                country_code       = v['country_code']
-                postal_code        = v['zip']
-                latitude           = v['latitude']
-                longitude          = v['longitude']
-                phone              = v['phone']
-            elif k == 'email':
-                email = v
-            elif k == 'customer':
-                first_name         = v['first_name']
-                last_name          = v['last_name']
-                shopify_id = v['id']
-            elif k == 'browser_ip':
-                ip = v
-            elif k == 'buyer_accepts_marketing':
-                accepts_marketing = v 
-            elif k == 'landing_site':
-                logging.info("LANDING SITE: %s" % v )
- 
-        # Fetch the order if we hve it.
-        o = OrderShopify.get_by_token( token )
-        if o == None:
-            user = get_or_create_user_by_email( email, self, client.apps.get() )
-        else:
-            user = o.user
- 
-        # Update the User's info
-        if user:
-            user.update(first_name         = first_name,
-                        last_name          = last_name,
-                        address1           = address1,
-                        address2           = address2,
-                        city               = city,
-                        province           = province,
-                        country_code       = country_code,
-                        postal_code        = postal_code,
-                        latitude           = latitude,
-                        longitude          = longitude,
-                        phone              = phone,
-                        email              = email,
-                        shopify_id         = shopify_id,
-                        ip                 = ip,
-                        accepts_marketing  = accepts_marketing)
-        else:
-            logging.error('NO USER: %s' % token)
+                # Store User/ Customer data
+                elif k == 'billing_address':
+                    address1           = v['address1']
+                    address2           = v['address2']
+                    city               = v['city']
+                    province           = v['province']
+                    country_code       = v['country_code']
+                    postal_code        = v['zip']
+                    latitude           = v['latitude']
+                    longitude          = v['longitude']
+                    phone              = v['phone']
+                elif k == 'email':
+                    email = v
+                elif k == 'customer':
+                    first_name         = v['first_name']
+                    last_name          = v['last_name']
+                    shopify_id = v['id']
+                elif k == 'browser_ip':
+                    ip = v
+                elif k == 'buyer_accepts_marketing':
+                    accepts_marketing = v 
+                elif k == 'landing_site':
+                    logging.info("LANDING SITE: %s" % v )
+     
+            # Fetch the order if we hve it.
+            o = OrderShopify.get_by_token( token )
+            if o == None:
+                user = get_or_create_user_by_email( email, self, client.apps.get() )
+            else:
+                user = o.user
+     
+            # Update the User's info
+            if user:
+                user.update(first_name         = first_name,
+                            last_name          = last_name,
+                            address1           = address1,
+                            address2           = address2,
+                            city               = city,
+                            province           = province,
+                            country_code       = country_code,
+                            postal_code        = postal_code,
+                            latitude           = latitude,
+                            longitude          = longitude,
+                            phone              = phone,
+                            email              = email,
+                            shopify_id         = shopify_id,
+                            ip                 = ip,
+                            accepts_marketing  = accepts_marketing)
+            else:
+                logging.error('NO USER: %s' % token)
 
-        if o == None:
-            # Make the Order
-            o = OrderShopify.create(
-                user,
-                client,
-                token,
-                order_id,
-                order_num, 
-                subtotal,
-                referring_site
-            )
-        else:
-            # Otherise, update it.
-            o.order_id = str(order_id)
-            o.order_number = str(order_num)
-            o.subtotal_price = float(subtotal)
-            o.referring_site = referring_site
+            if o == None:
+                # Make the Order
+                o = OrderShopify.create(
+                    user,
+                    client,
+                    token,
+                    order_id,
+                    order_num, 
+                    subtotal,
+                    referring_site
+                )
+            else:
+                # Otherise, update it.
+                o.order_id = str(order_id)
+                o.order_number = str(order_num)
+                o.subtotal_price = float(subtotal)
+                o.referring_site = referring_site
 
-        # Store the purchased items in the order
-        o.products.extend( items )
-        o.put()
+            # Store the purchased items in the order
+            o.products.extend( items )
+            o.put()
+    except Exception, e:
+        logging.error("Error occurred during Order processing: %s" % e)
 
