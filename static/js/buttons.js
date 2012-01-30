4873336cfa629065f038d7a7b5c7f4c9bc9109ed
Barbara <z4beth@gmail.com>
2012-01-30 15:46:32 -0500

    Applying comit effe1d75e42347ea60147130f147bcd1754a28dc without cherry picking to avoid merge conflicts.

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -1,8 +1,6 @@
 /**
  * Buttons JS. Copyright Willet Inc, 2012
  */
-<<<<<<< HEAD
-=======
 (function () {
     var here = window.location + '.json';
     var console = ( typeof(window.console) === 'object' 
@@ -51,194 +49,114 @@
         if (button_div && window._willet_iframe_loaded == undefined) {
             console.log("Buttons: found placeholder, attaching iframe");
 
-            button_div.setAttribute('style',"float: left; height: 30px; padding: 5px;");
->>>>>>> 4d2fa59... JSON added to buttons.js for faster loading
+            button_div.style.styleFloat = 'left'; // IE
+            button_div.style.cssFloat = 'left'; // FF, Webkit
+            button_div.style.width = '240px';
+            button_div.style.height = '30px';
+            button_div.style.padding = '5px';
 
-/**
-* quick helper function to add scripts to dom
-*/
-var _willet_load_remote_script = function(script) {
-    var dom_el  = document.createElement('script'); 
-    dom_el.src  = script;
-    dom_el.type = 'text/javascript'; 
-    document.getElementsByTagName('head')[0].appendChild(dom_el); 
-    return dom_el;
-};
+            window._willet_iframe_loaded = "teh fb_iframe haz been loaded";
+            var domain = /:\/\/([^\/]+)/.exec(window.location.href)[1];
 
-var scripts = [
-/**
-* Scripts to load into the dom
-*   name - name of the script
-*   url - url of script
-*   dom_el - dom element once inserted
-*   loaded - script has been loaded
-*   test - method to test if it has been loaded
-*   callback - callback after test is success
-*/
-    {
-        'name': 'jQuery',
-        'url': 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js',
-        'dom_el': null,
-        'loaded': false,
-        'test': function() {
-            return (typeof jQuery == 'function')
-        }, 'callback': function() {
-            // HACKETY HACK HACK
-            $ = jQuery;
-        }
-    }
-];
-
-var _willet_check_scripts = function() {
-    /**
-    * checkScripts checks the scripts var and uses
-    * the defined `test` and `callack` methods to tell
-    * when a script has been loaded and is ready to be
-    * used
-    */    
-    var all_loaded = true;
-
-    for (i = 0; i < scripts.length; i++) {
-        var row  = scripts[i];
-        if (row.dom_el == null) {
-            // insert the script into the dom
-            if (row.test()) {
-                // script is already loaded!
-                row.callback();
-                row.loaded = true;
-                row.dom_el = true;
-            } else {
-                row.dom_el = _willet_load_remote_script(row.url);
+            // Grab the photo
+            var photo = '';
+            if ( data.product.images[0] != null ) {
+                photo = data.product.images[0].src;
             }
-        }
-        if (row.loaded == false) {
-            if (row.test()) {
-                // script is now loaded!
-                row.callback();
-                row.loaded = true;
-            } else {
-                all_loaded = false;
+
+            // Tumblr
+            var d = document.createElement( 'div' );
+            d.style.styleFloat = 'left'; // IE
+            d.style.cssFloat = 'left'; // FF, Webkit
+            d.style.marginRight = '5px';
+            d.style.width = '62px';
+            
+            var a = document.createElement( 'a' );
+            a.href = 'http://www.tumblr.com/share';
+            a.title = "Share on Tumblr";
+            a.style.display = 'inline-block';
+            a.style.textIndent = '-9999px';
+            a.style.overflow = 'hidden';
+            a.style.width = '63px';
+            a.style.height = '20px';
+            a.style.background = "url('http://platform.tumblr.com/v1/share_2.png') top left no-repeat transparent";
+            a.style.styleFloat = 'left'; // IE
+            a.style.cssFloat = 'left'; // FF, Webkit
+            a.style.marginRight = '5px';
+            a.style.marginTop = '3px';
+            a.innerHTML = "Share on Tumblr";
+            d.appendChild( a );
+            button_div.appendChild( d );
+
+            console.log('Buttons: Tumblr attached');
+
+            // Pinterest
+            d = document.createElement( 'div' );
+            d.style.styleFloat = 'left'; // IE
+            d.style.cssFloat = 'left'; // FF, Webkit
+            d.style.marginRight = '5px';
+            d.style.overflow = 'hidden'; 
+            d.style.width = '49px';
+
+            a = document.createElement( 'a' );
+            a.href = "http://pinterest.com/pin/create/button/?" +
+                    "url=" + encodeURIComponent( window.location.href ) + 
+                    "&media=" + encodeURIComponent( photo ) + 
+                    "&description=" + "Found on " + domain + "!";
+            a.className = 'pin-it-button';
+            a.setAttribute('count-layout', "horizontal");
+            a.innerHTML = "Pin It";
+            d.appendChild( a );
+            button_div.appendChild( d );
+            
+            console.log("Buttons: Pinterest attached");
+
+            // The Fancy Button
+            d = document.createElement( 'div' );
+            d.style.styleFloat = 'left'; // IE
+            d.style.cssFloat = 'left'; // FF, Webkit
+            d.style.marginTop = '3px';
+
+            a = document.createElement( 'a' );
+            var u = "http://www.thefancy.com/fancyit?" +
+                    "ItemURL=" + encodeURIComponent( window.location.href ) + 
+                    "&Title="  + encodeURIComponent( data.product.title ) +
+                    "&Category=Other";
+            if ( photo.length > 0 ) {
+                u += "&ImageURL=" + encodeURIComponent( photo );
+            } else { // If no image on page, submit blank image.
+                u += "&ImageURL=" + encodeURIComponent( 'http://social-referral.appspot.com/static/imgs/noimage.png' );
             }
-        }   
-    }
 
-    if (all_loaded) {
-        // let's actually get this party started 
-        _willet_run_scripts();
-    } else {
-        window.setTimeout(_willet_check_scripts,100);
-    }
-};
+            a.href = u;
+            a.id = 'FancyButton';
+            d.appendChild( a );
+            button_div.appendChild( d );
 
-/**
- * body of code to run when all scripts have been injected
- */
-var _willet_run_scripts = function() {
-    // get the button where we are going to insert
-    var here = window.location + '.json';
-    var $    = jQuery;
-
-    jQuery.getJSON (
-        here,
-        function(data) {
-            // callback function
+            console.log('Buttons: Fancy attached.');
+            console.log('Buttons: appending button scripts');
+            
             var head = document.getElementsByTagName('head')[0];
             var tmp  = document.createElement( 'script' );
-            $(tmp).attr( 'type', 'text/javascript' );
-            $(tmp).attr( 'src', 'http://assets.pinterest.com/js/pinit.js' );
+            tmp.type = 'text/javascript';
+            tmp.src = 'http://assets.pinterest.com/js/pinit.js';
             head.appendChild( tmp );
             
             tmp = document.createElement( 'script' );
-            $(tmp).attr( 'type', 'text/javascript' );
-            $(tmp).attr( 'src', 'http://platform.tumblr.com/v1/share.js' );
+            tmp.type = 'text/javascript';
+            tmp.src = 'http://platform.tumblr.com/v1/share.js';
             head.appendChild( tmp );
 
             tmp = document.createElement( 'script' );
-            $(tmp).attr( 'type', 'text/javascript' );
-            $(tmp).attr( 'src', 'http://www.thefancy.com/fancyit.js' );
+            tmp.type = 'text/javascript';
+            tmp.src = 'http://www.thefancy.com/fancyit.js';
             head.appendChild( tmp );
 
-            /**
-             * INSERT IFRAME WITH DATA
-             */
-            var button_div = document.getElementById('_willet_buttons_app');
-
-            if (button_div && window.iframe_loaded == undefined) {
-                $(button_div).css( {"float"   : "left",
-                                    "height"  : "30px",
-                                   // "width"   : "225px",
-                                    "padding" : "5px"} );
-
-                window.iframe_loaded = "teh fb_iframe haz been loaded";
-                var domain = /:\/\/([^\/]+)/.exec(window.location.href)[1];
-
-<<<<<<< HEAD
-                // Grab the photo
-                var photo = '';
-                if ( data.product.images[0] != null ) {
-                    photo = data.product.images[0].src;
-                }
-
-                // Tumblr
-                var d = document.createElement( 'div' );
-                $(d).attr('style', 'float: left; margin-right: 5px; width: 62px !important;' );
-                
-                var a = document.createElement( 'a' );
-                var style = "display:inline-block; text-indent:-9999px; " +
-                            "overflow:hidden; width:63px; height:20px; " + 
-                            "background:url('http://platform.tumblr.com/v1/" + 
-                            "share_2.png') top left no-repeat transparent; float: left; margin-right: 5px; margin-top: 3px;" 
-                $(a).attr( 'href', 'http://www.tumblr.com/share' );
-                $(a).attr( 'title', "Share on Tumblr" );
-                $(a).attr( 'style', style);
-                $(a).html = "Share on Tumblr";
-                d.appendChild( a );
-                button_div.appendChild( d );
-
-                // Pinterest
-                var d = document.createElement( 'div' );
-                $(d).attr('style', 'float: left; margin-right: 5px; overflow:hidden; width: 49px !important;' );
-
-                a = document.createElement( 'a' );
-                var u = "http://pinterest.com/pin/create/button/?" +
-                        "url=" + encodeURIComponent( window.location.href ) + 
-                        "&media=" + encodeURIComponent( photo ) + 
-                        "&description=" + "Found on " + domain + "!";
-                $(a).attr( 'href', u );
-                $(a).attr( 'class', 'pin-it-button' );
-                $(a).attr( 'count-layout', "horizontal" );
-                $(a).html = "Pin It";
-                d.appendChild( a );
-                button_div.appendChild( d );
-
-                // The Fancy Button
-                var d = document.createElement( 'div' );
-                $(d).attr('style', 'float: left; margin-top: 3px;' );
-
-                a = document.createElement( 'a' );
-                var u = "http://www.thefancy.com/fancyit?" +
-                        "ItemURL=" + encodeURIComponent( window.location.href ) + 
-                        "&Title="  + encodeURIComponent( data.product.title ) +
-                        "&Category=Other";
-                if ( photo.length > 0 ) {
-                    u += "&ImageURL=" + encodeURIComponent( photo );
-                } else { // If no image on page, submit blank image.
-                    u += "&ImageURL=" + encodeURIComponent( 'http://social-referral.appspot.com/static/imgs/noimage.png' );
-                }
-
-                $(a).attr( 'href', u );
-                $(a).attr( 'id', 'FancyButton' );
-                d.appendChild( a );
-                button_div.appendChild( d ); 
-            }
+            console.log('Buttons: scripts attached. Done!');
+        } else {
+            console.error('Buttons: could not find buttons placeholder on page');
         }
-    );
-};
 
-// let's start to get this party started 
-_willet_check_scripts();
-
-=======
     };
 
     // Get product info, then load scripts
@@ -248,7 +166,8 @@ _willet_check_scripts();
             var req = new XMLHttpRequest();
             req.open('GET', here, true);
             req.onreadystatechange = function () {  
-                if (req.readyState === 4) {  
+                if (req.readyState === 4) {
+                    // 4 means something has been returned by the request
                     if (req.status === 200) {
                         console.log("Buttons: recieved product.json request");
                         var data;
@@ -275,4 +194,3 @@ _willet_check_scripts();
         }
     })();
 })();
->>>>>>> 4d2fa59... JSON added to buttons.js for faster loading
