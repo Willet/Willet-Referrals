4d2fa59a68a51498a5d5531f1d398b0a42e03eec
Fraser Harris <fraser.harris@gmail.com>
2012-01-30 13:32:37 -0500

    JSON added to buttons.js for faster loading

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -1,5 +1,5 @@
 /**
- * Buttons JS
+ * Buttons JS. Copyright Willet Inc, 2012
  */
 (function () {
     var here = window.location + '.json';
@@ -8,27 +8,34 @@
                  && typeof(window.console.error) ==='function' ) 
                     ? window.console 
                     : { log: function () {}, error: function () {} };
-    var scripts = [
-        /**
-        * Scripts to load into the dom
-        *   name - name of the script
-        *   url - url of script
-        *   dom_el - dom element once inserted
-        *   loaded - script has been loaded
-        *   test - method to test if it has been loaded
-        *   callback - callback after test is success
-        */
-        {
-            'name': 'json',
-            'url': 'http://cdnjs.cloudflare.com/ajax/libs/json2/20110223/json2.js',
-            'dom_el': null,
-            'loaded': false,
-            'test': function() {
-                return (typeof(window.JSON) === 'object' && typeof(window.JSON.parse) === 'function');
-            },
-            'callback': function() {}
-        }
-    ];
+    
+    var JSON;if(!JSON){JSON={};}
+    /* JSON2, Author: Douglas Crockford, http://www.JSON.org/json2.js */
+    (function(){'use strict';function f(n){return n<10?'0'+n:n;}
+    if(typeof Date.prototype.toJSON!=='function'){Date.prototype.toJSON=function(key){return isFinite(this.valueOf())?this.getUTCFullYear()+'-'+
+    f(this.getUTCMonth()+1)+'-'+
+    f(this.getUTCDate())+'T'+
+    f(this.getUTCHours())+':'+
+    f(this.getUTCMinutes())+':'+
+    f(this.getUTCSeconds())+'Z':null;};String.prototype.toJSON=Number.prototype.toJSON=Boolean.prototype.toJSON=function(key){return this.valueOf();};}
+    var cx=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,escapable=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,gap,indent,meta={'\b':'\\b','\t':'\\t','\n':'\\n','\f':'\\f','\r':'\\r','"':'\\"','\\':'\\\\'},rep;function quote(string){escapable.lastIndex=0;return escapable.test(string)?'"'+string.replace(escapable,function(a){var c=meta[a];return typeof c==='string'?c:'\\u'+('0000'+a.charCodeAt(0).toString(16)).slice(-4);})+'"':'"'+string+'"';}
+    function str(key,holder){var i,k,v,length,mind=gap,partial,value=holder[key];if(value&&typeof value==='object'&&typeof value.toJSON==='function'){value=value.toJSON(key);}
+    if(typeof rep==='function'){value=rep.call(holder,key,value);}
+    switch(typeof value){case'string':return quote(value);case'number':return isFinite(value)?String(value):'null';case'boolean':case'null':return String(value);case'object':if(!value){return'null';}
+    gap+=indent;partial=[];if(Object.prototype.toString.apply(value)==='[object Array]'){length=value.length;for(i=0;i<length;i+=1){partial[i]=str(i,value)||'null';}
+    v=partial.length===0?'[]':gap?'[\n'+gap+partial.join(',\n'+gap)+'\n'+mind+']':'['+partial.join(',')+']';gap=mind;return v;}
+    if(rep&&typeof rep==='object'){length=rep.length;for(i=0;i<length;i+=1){if(typeof rep[i]==='string'){k=rep[i];v=str(k,value);if(v){partial.push(quote(k)+(gap?': ':':')+v);}}}}else{for(k in value){if(Object.prototype.hasOwnProperty.call(value,k)){v=str(k,value);if(v){partial.push(quote(k)+(gap?': ':':')+v);}}}}
+    v=partial.length===0?'{}':gap?'{\n'+gap+partial.join(',\n'+gap)+'\n'+mind+'}':'{'+partial.join(',')+'}';gap=mind;return v;}}
+    if(typeof JSON.stringify!=='function'){JSON.stringify=function(value,replacer,space){var i;gap='';indent='';if(typeof space==='number'){for(i=0;i<space;i+=1){indent+=' ';}}else if(typeof space==='string'){indent=space;}
+    rep=replacer;if(replacer&&typeof replacer!=='function'&&(typeof replacer!=='object'||typeof replacer.length!=='number')){throw new Error('JSON.stringify');}
+    return str('',{'':value});};}
+    if(typeof JSON.parse!=='function'){JSON.parse=function(text,reviver){var j;function walk(holder,key){var k,v,value=holder[key];if(value&&typeof value==='object'){for(k in value){if(Object.prototype.hasOwnProperty.call(value,k)){v=walk(value,k);if(v!==undefined){value[k]=v;}else{delete value[k];}}}}
+    return reviver.call(holder,key,value);}
+    text=String(text);cx.lastIndex=0;if(cx.test(text)){text=text.replace(cx,function(a){return'\\u'+
+    ('0000'+a.charCodeAt(0).toString(16)).slice(-4);});}
+    if(/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,'@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,']').replace(/(?:^|:|,)(?:\s*\[)+/g,''))){j=eval('('+text+')');return typeof reviver==='function'?walk({'':j},''):j;}
+    throw new SyntaxError('JSON.parse');};}}());
+
 
     var _init_buttons = function(data) {
         /* data is product json 
@@ -135,64 +142,8 @@
 
     };
 
-    var _load_remote_script = function(script) {
-        /**
-         * quick helper function to add scripts to dom
-        */
-        console.log("Buttons: loading "+script);
-        var dom_el  = document.createElement('script'); 
-        dom_el.src  = script;
-        dom_el.type = 'text/javascript'; 
-        document.getElementsByTagName('head')[0].appendChild(dom_el); 
-        return dom_el;
-    };
-
-    var _check_scripts = function() {
-        /**
-        * checkScripts checks the scripts var and uses
-        * the defined `test` and `callack` methods to tell
-        * when a script has been loaded and is ready to be
-        * used
-        */    
-        var all_loaded = true;
-        console.log("Buttons: Loading JSON begins");
-
-        for (i = 0; i < scripts.length; i++) {
-            var row  = scripts[i];
-            if (row.dom_el == null) {
-                // insert the script into the dom
-                if (row.test()) {
-                    // script is already loaded!
-                    row.callback();
-                    row.loaded = true;
-                    row.dom_el = true;
-                } else {
-                    row.dom_el = _load_remote_script(row.url);
-                }
-            }
-            if (row.loaded == false) {
-                if (row.test()) {
-                    // script is now loaded!
-                    row.callback();
-                    row.loaded = true;
-                } else {
-                    all_loaded = false;
-                }
-            }   
-        }
-
-        if (all_loaded) {
-            // let's actually get this party started
-            console.log("Buttons: JSON library loaded, run script");
-            _run_scripts();
-        } else {
-            console.log("Buttons: JSON library loading...");
-            window.setTimeout(_check_scripts,100);
-        }
-    };
-
-    // Run once JSON is ready
-    var _run_scripts = function() {
+    // Get product info, then load scripts
+    (function() {
         try {
             console.log("Buttons: initiating product.json request")
             var req = new XMLHttpRequest();
@@ -206,8 +157,10 @@
                             data = JSON.parse(req.responseText);
                         } catch (e) {
                             console.error("Buttons: JSON.parse error: "+e);
+                            return;
                         }
                         if (data) {
+                            // Proceed!
                             _init_buttons(data);
                         }
                     } else {  
@@ -221,15 +174,5 @@
             // Didn't work, just silently bail
             console.error("Buttons: "+e);
         }
-    };
-
-    if (typeof (window.JSON) === 'object' && typeof(window.JSON.parse) === 'function') {
-        // let's get this party started
-        console.log("Buttons: JSON found, run script");
-        _run_scripts();
-    } else {
-        // go get JSON library
-        console.log("Buttons: no JSON, load JSON");
-        _check_scripts();
-    }
+    })();
 })();
