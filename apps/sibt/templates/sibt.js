7d052bad955a41ac46c7d643fd8cf19835cb5463
Brian Lai <b2lai@uwaterloo.ca>
2012-02-21 16:47:25 -0500

    fix  without b param

--- apps/sibt/templates/sibt.js
+++ apps/sibt/templates/sibt.js
@@ -4,7 +4,7 @@
 
 // http://blog.fedecarg.com/2011/07/12/javascript-asynchronous-script-loading-and-lazy-loading/
 // $L ([js files], function () { after_code });
-$L=function(a,b){for(var c=a.length,d=c,e=function(){if(!(this.readyState&&this.readyState!=="complete"&&this.readyState!=="loaded")){this.onload=this.onreadystatechange=null;--d||b()}},f=document.getElementsByTagName("head")[0],g=function(a){var b=document.createElement("script");b.async=true;b.src=a;b.onload=b.onreadystatechange=e;f.appendChild(b)};c;)g(a[--c])}
+var $L=function(a,b){b = b||function(){};for(var c=a.length,d=c,e=function(){if(!(this.readyState&&this.readyState!=="complete"&&this.readyState!=="loaded")){this.onload=this.onreadystatechange=null;--d||b()}},f=document.getElementsByTagName("head")[0],g=function(a){var b=document.createElement("script");b.async=true;b.src=a;b.onload=b.onreadystatechange=e;f.appendChild(b)};c;)g(a[--c])}
 
 // Fun Safari cookie hack ... wooooo
 var firstTimeSession = 0;
