287668bff0fd753ff563b165bbf4bae6a7373a20
Fraser Harris <fraser.harris@gmail.com>
2012-02-22 02:18:25 -0500

    Shopify billing methods that mirror API done

---
+++ util/errors.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# Exceptions
+
+__author__      = "Willet, Inc."
+__copyright__   = "Copyright 2012, Willet, Inc"
+
+
+class RemoteError(Exception):
+	""" Exception raised when a HTTP fetch returns fails or returns an unexpected results"""
+
+	def __init__(self, HTTP_status_code, HTTP_status_msg, description):
+		self.status = HTTP_status_code
+		self.name = HTTP_status_msg
+		self.description = description
+	
+	def __str__(self):
+		return "%s %s: %s" % (self.status, self.name, self.description)
+
+class ShopifyAPIError(RemoteError):
+	""" A more descriptive exception for Shopify """
+	pass
+
+class BillingError(Exception):
+	""" Exception raised when a billing API request fails or returns an unexpected result"""
+
+	def __init__(self, message, data):
+		self.message = message
+		self.data = data
+
+	def __str__(self):
+		return "%s\ndata: %r" % (self.message, self.data)
+
+class ShopifyBillingError(BillingError):
+	pass
