8b5bc5edc3f1ec2f3eb6829c3b2e74b854f6885f
Brian Lai <b2lai@uwaterloo.ca>
2012-03-19 11:17:22 -0400

    change domain

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -105,11 +105,11 @@
                         d.id = 'mini_sibt_button';
                         d.style.cursor = 'pointer';
                         d.style.display = 'inline-block';
-                        d.style.background = "url('" + protocol + "//brian-willet2.appspot.com/static/sibt/imgs/button_bkg.png') 3% 20% no-repeat transparent";
+                        d.style.background = "url('" + protocol + "//brian-willet.appspot.com/static/sibt/imgs/button_bkg.png') 3% 20% no-repeat transparent";
                         d.style.width = '80px';
                         return d;
                     },
-                    script: protocol+'//brian-willet2.appspot.com/s/shopify/sibt-buttons.js?store_url=' + store_url
+                    script: protocol+'//brian-willet.appspot.com/s/shopify/sibt-buttons.js?store_url=' + store_url
                 },
                 Tumblr: {
                     create: function () {
