4ea6037a881545603da1054f19186c6b096dab2e
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2012-04-09 10:28:11 -0400

    Fixed warnings in PyCharm and refactored
    - Ensured all classes / methods had docstrings
    - Removed unused imports
    - Fixed style (lines are now less than 80 columns long)
    - Added method to get common details
    - Added decorator to handle try / catch blocks

--- apps/buttons/shopify/views.py
+++ apps/buttons/shopify/views.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
+""" 'Get' functions for the ShopConnection application
+"""
 
-import logging
-
-from google.appengine.ext           import webapp
-from google.appengine.ext.webapp    import template
-from urlparse                       import urlparse
-
+from logging                        import error
 from apps.buttons.shopify.models    import ButtonsShopify 
 from apps.client.shopify.models     import ClientShopify
 from util.consts                    import *
@@ -14,9 +11,68 @@ from util.urihandler                import URIHandler
 from apps.email.models              import Email
 from util.helpers                   import url as build_url
 
+def catch_error(fn):
+    """Decorator for catching errors in ButtonsShopify install
+    """
+    def wrapped(self):
+        """Wrapped function, assumed to be a URI Handler
+        """
+        client_email = ''
+        shop_owner   = ''
+        shop_name    = ''
+        shop_url     = ''
+        try:
+            details = get_details(self)
+            client_email = details["client_email"]
+            shop_owner   = details["shop_owner"]
+            shop_name    = details["shop_name"]
+            shop_url     = details["shop_url"]
+
+            fn(self)
+        except Exception, e:
+            error('Smart-buttons install error, may require reinstall',
+                  exc_info=True)
+
+            # Email DevTeam
+            Email.emailDevTeam(
+                'Smart-buttons install error, may require reinstall: '\
+                '%s, %s, %s, %s' %
+                (client_email, shop_owner, shop_url, shop_name)
+            )
+            self.redirect ("%s?reason=%s" %
+                           (build_url ('ButtonsShopifyInstallError'), e))
+    return wrapped
+
+def get_details(uri_handler=None, provided_client=None):
+    """ Given a URIHandler, returns a dictionary of details we expect
+    """
+    if uri_handler is None and uri_handler.request is None:
+        raise RuntimeError("No URI Handler provided!")
+
+    request = uri_handler.request
+
+    details = dict()
+    details["shop_url"] = request.get("shop") or request.get("shop_url")
+
+    client = provided_client or ClientShopify.get_by_url(details["shop_url"])
+    if client is not None and client.merchant is not None:
+        details["client_email"] = client.email
+        details["shop_owner"]   = client.merchant.get_full_name()
+        details["shop_name"]    = client.name
+        details["client"]       = client
+    else:
+        details["client_email"] = ""
+        details["shop_owner"]   = "Shopify Merchant"
+        details["shop_name"]    = "Your Shopify Store"
+        details["client"]       = ""
+
+    return details
+
 class ButtonsShopifyBeta(URIHandler):
     """ If an existing customer clicks through from Shopify """
     def get(self):
+        """ Display the default 'welcome' page
+        """
         template_values = {
             "SHOPIFY_API_KEY": SHOPIFY_APPS['ButtonsShopify']['api_key']
         }
@@ -25,193 +81,149 @@ class ButtonsShopifyBeta(URIHandler):
 
 class ButtonsShopifyWelcome(URIHandler):
     """ After the installation process, provide an opportunity to upgrade"""
+    @catch_error
     def get(self):
-        client_email = ''
-        shop_owner = ''
-        shop_name = ''
-        shop_url = ''
-        try:
-            # TODO: put this somewhere smarter
-            shop_url = self.request.get( 'shop' )
-            token = self.request.get( 't' )
+        """ Display upgrade options
+        """
+        token = self.request.get( 't' )
+        details = get_details(self)
 
-            # Fetch the client
-            client       = ClientShopify.get_by_url( shop_url )
-            shop_owner   = 'Shopify Merchant'
-            shop_name    = 'Your Shopify Store'
-            if client is not None and client.merchant is not None:
-                client_email = client.email
-                shop_owner   = client.merchant.get_full_name()
-                shop_name    = client.name
+        # Fetch or create the app
+        app = ButtonsShopify.get_or_create_app(details["client"], token=token)
 
-            # Fetch or create the app
-            app = ButtonsShopify.get_or_create_app(client, token=token)
+        # Find out what the app should cost
+        price = app.get_price()
 
-            # Find out what the app should cost
-            price = app.get_price()
+        template_values = {
+            'app'        : app,
+            'shop_owner' : details["shop_owner"],
+            'shop_name'  : details["shop_name"],
+            'price'      : price,
+            'shop_url'   : details["shop_url"],
+            'token'      : token,
+        }
 
-            template_values = {
-                'app'        : app,
-                'shop_owner' : shop_owner,
-                'shop_name'  : shop_name,
-                'price'      : price,
-                'shop_url'   : shop_url,
-                'token'      : token,
-            }
-            
-            self.response.out.write(self.render_page('upsell.html', template_values))
-        except Exception, e:
-            logging.error('Smart-buttons install error, may require reinstall', exc_info=True)
-            # Email DevTeam
-            Email.emailDevTeam(
-                'Smart-buttons install error, may require reinstall: %s, %s, %s, %s' % 
-                    (client_email, shop_owner, shop_url, shop_name)
-            )
-            self.redirect ("%s?reason=%s" % (build_url ('ButtonsShopifyInstallError'), e))
-            return
+        self.response.out.write(self.render_page('upsell.html',
+                                                 template_values))
 
 class ButtonsShopifyUpgrade(URIHandler):
     """ Starts the upgrade process """
+    @catch_error
     def get(self):
-        client_email = ''
-        shop_owner = ''
-        shop_name = ''
-        shop_url = ''
-        try:
-            shop_url = self.request.get("shop_url")
+        """ Begin the upgrade process
+        """
+        details = get_details(self)
 
-            existing_app = ButtonsShopify.get_by_url(shop_url)
-            if not existing_app:
-                logging.error("error calling billing callback: 'existing_app' not found. Install first?")
+        existing_app = ButtonsShopify.get_by_url(details["shop_url"])
+        if not existing_app:
+            error("error calling billing callback: "
+                          "'existing_app' not found. Install first?")
 
-            # Fetch the client
-            client       = ClientShopify.get_by_id(existing_app.store_id)
-            shop_owner   = 'Shopify Merchant'
-            shop_name    = 'Your Shopify Store'
-            if client is not None and client.merchant is not None:
-                client_email = client.email
-                shop_owner   = client.merchant.get_full_name()
-                shop_name    = client.name
-                shop_url     = client.url 
+        if existing_app.recurring_billing_price:
+            price = existing_app.recurring_billing_price
+        else:
+            price = existing_app.get_price()
 
-            if existing_app.recurring_billing_price:
-                price = existing_app.recurring_billing_price
-            else:
-                price = existing_app.get_price()
+        # Start the billing process
+        confirm_url = existing_app.setup_recurring_billing({
+            "price":        price,
+            "name":         "ShopConnection",
+            "return_url":   "%s/b/shopify/billing_callback?app_uuid=%s" %
+                            (URL, existing_app.uuid),
+            "test":         USING_DEV_SERVER
+        })
 
-            # Start the billing process
-            confirm_url = existing_app.setup_recurring_billing({
-                "price":        price,
-                "name":         "ShopConnection",
-                "return_url":   "%s/b/shopify/billing_callback?app_uuid=%s" % (URL, existing_app.uuid),
-                "test":         USING_DEV_SERVER
-            })
+        existing_app.put()
 
-            existing_app.put()
-
-            if confirm_url:
-                self.redirect(confirm_url)
-                return
-            else:
-                # Can this even occur?
-                raise ShopifyBillingError('No confirmation URL provided by Shopify API', {})
-
-        except Exception, e:
-            logging.error('Smart-buttons install error, may require reinstall', exc_info=True)
-            # Email DevTeam
-            Email.emailDevTeam(
-                'Smart-buttons install error, may require reinstall: %s, %s, %s, %s' % 
-                    (client_email, shop_owner, shop_url, shop_name)
-            )
-            self.redirect ("%s?reason=%s" % (build_url ('ButtonsShopifyInstallError'), e))
+        if confirm_url:
+            self.redirect(confirm_url)
             return
+        else:
+            # Can this even occur?
+            raise ShopifyBillingError('No confirmation URL provided by '
+                                      'Shopify API', {})
 
 class ButtonsShopifyBillingCallback(URIHandler):
     """ When a customer confirms / denies billing, they are redirected here
 
-    Activates billing with Shopify, then redirects customer to installation instructions
+    Activates billing with Shopify, then redirects customer to installation
+    instructions
     """
+    @catch_error
     def get(self):
-        client_email = ''
-        shop_owner = ''
-        shop_name = ''
-        shop_url = ''
-        try:
-            app_uuid =self.request.get('app_uuid')
-            app = ButtonsShopify.get_by_uuid(app_uuid)
+        """ Activate the billing charges after Shopify has setup the charge
+        """
 
-            # Fetch the client
-            client       = ClientShopify.get_by_id(app.store_id)
-            shop_owner   = 'Shopify Merchant'
-            shop_name    = 'Your Shopify Store'
-            if client is not None and client.merchant is not None:
-                client_email = client.email
-                shop_owner   = client.merchant.get_full_name()
-                shop_name    = client.name
-                shop_url     = client.url 
-
-            if not app:
-                logging.error("error calling billing callback: 'app' not found")
-
-            charge_id = int(self.request.get('charge_id'))
-
-            if charge_id == app.recurring_billing_id:
-                # Good to go, activate!
-                app.activate_recurring_billing({
-                    'return_url': self.request.url,
-                    'test': 'true'
-                })
-                app.billing_enabled = True
-
-                app.put()
-
-                app.do_upgrade()
-            else:
-                raise ShopifyBillingError('Charge id in request does not match expected charge id', app.recurring_billing_id)
-            
-            # Render the page
-            template_values = {
-                'app'        : app,
-                'shop_owner' : shop_owner,
-                'shop_name'  : shop_name
-            }
-            self.response.out.write(self.render_page('welcome.html', template_values))
-
-        except Exception, e:
-            logging.error('Smart-buttons install error, may require reinstall', exc_info=True)
-            # Email DevTeam
-            Email.emailDevTeam(
-                'Smart-buttons install error, may require reinstall: %s, %s, %s, %s' % 
-                    (client_email, shop_owner, shop_url, shop_name)
-            )
-            self.redirect ("%s?reason=%s" % (build_url ('ButtonsShopifyInstallError'), e))
-            return
-
-class ButtonsShopifyInstructions(URIHandler):
-    def get( self ):
-        # TODO: put this somewhere smarter
-        shop   = self.request.get( 'shop' )
-        token  = self.request.get( 't' )
+        app_uuid =self.request.get('app_uuid')
+        app = ButtonsShopify.get_by_uuid(app_uuid)
 
         # Fetch the client
-        client = ClientShopify.get_by_url( shop )
-    
+        # Since we cannot get the client until we have the app, in this case,
+        # we pass in the client
+        client  = ClientShopify.get_by_id(app.store_id)
+        details = get_details(self, provided_client=client)
+
+
+        if not app:
+            error("error calling billing callback: 'app' not found")
+
+        charge_id = int(self.request.get('charge_id'))
+
+        if charge_id == app.recurring_billing_id:
+            # Good to go, activate!
+            app.activate_recurring_billing({
+                'return_url': self.request.url,
+                'test': 'true'
+            })
+            app.billing_enabled = True
+
+            app.put()
+
+            app.do_upgrade()
+        else:
+            raise ShopifyBillingError('Charge id in request does not match '
+                                      'expected charge id',
+                                      app.recurring_billing_id)
+
+        # Render the page
+        template_values = {
+            'app'        : app,
+            'shop_owner' : details["shop_owner"],
+            'shop_name'  : details["shop_name"]
+        }
+        self.response.out.write(self.render_page('welcome.html',
+                                                 template_values))
+
+class ButtonsShopifyInstructions(URIHandler):
+    """ Actions for the instructions page
+    """
+    @catch_error
+    def get( self ):
+        """ Displays post-installation instructions
+        """
+        details = get_details(self)
+        token  = self.request.get( 't' )
+
         # Fetch or create the app
-        app    = ButtonsShopify.get_or_create_app(client, token=token)
+        app    = ButtonsShopify.get_or_create_app(details["client"],
+                                                  token=token)
 
         # Render the page
         template_values = {
             'app'          : app,
-            'shop_owner'   : client.merchant.get_full_name(),
-            'shop_name'    : client.name
+            'shop_owner'   : details["shop_owner"],
+            'shop_name'    : details["shop_name"]
         }
 
-        self.response.out.write(self.render_page('welcome.html', template_values))
+        self.response.out.write(self.render_page('welcome.html',
+                                                 template_values))
 
 class ButtonsShopifyInstallError(URIHandler):
+    """ Actions for the error page
+    """
     def get (self):
-        """ Displays an error page for when the Buttons app fails to install or upgrade. 
-            Error emails are not handled by this page. 
+        """ Displays an error page for when the Buttons app fails to install
+            or upgrade. Error emails are not handled by this page.
         """
         
         template_values = {
@@ -220,5 +232,6 @@ class ButtonsShopifyInstallError(URIHandler):
         }
         self.response.headers.add_header('P3P', P3P_HEADER)
         self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-        self.response.out.write(self.render_page('install_error.html', template_values))
+        self.response.out.write(self.render_page('install_error.html',
+                                                 template_values))
         return
