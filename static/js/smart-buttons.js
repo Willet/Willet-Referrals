6b4c59b125ff43876c0c3b6e6265bace2c9bc2b4
Fraser Harris <fraser.harris@gmail.com>
2012-04-17 17:18:11 -0400

    Fixed string.indexOf bug

--- static/js/smart-buttons.js
+++ static/js/smart-buttons.js
@@ -726,7 +726,7 @@ _willet.cookies = (function (helpers) {
         for(var i=0;i < ca.length;i++) {
             var c = ca[i];
             while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (helpers.indexOf(c, nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
         }
         return null;
     };
