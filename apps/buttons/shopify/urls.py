69ce238cde4ad0ccdc96bb928bf144a8a9815240
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2012-04-25 11:27:12 -0400

    Moved email functionality to processes.py

--- apps/buttons/shopify/urls.py
+++ apps/buttons/shopify/urls.py
@@ -3,16 +3,16 @@ from apps.buttons.shopify.processes import *
 from apps.buttons.shopify.views import *
 
 urlpatterns = [
-	# Views
-    (r'/b/shopify/beta',        		ButtonsShopifyBeta),
-    (r'/b/shopify/',            		ButtonsShopifyBeta),
-    (r'/b/shopify',             		ButtonsShopifyBeta),
-    (r'/b/shopify/learn',				ButtonsShopifyLearn),
-    (r'/b/shopify/welcome', 			ButtonsShopifyWelcome),
-    (r'/b/shopify/upgrade', 			ButtonsShopifyUpgrade),
-    (r'/b/shopify/billing_callback', 	ButtonsShopifyBillingCallback),
-    (r'/b/shopify/instructions',     	ButtonsShopifyInstructions),
-    (r'/b/shopify/error.html',     		ButtonsShopifyInstallError),
+    # Views
+    (r'/b/shopify/beta',                ButtonsShopifyBeta),
+    (r'/b/shopify/',                    ButtonsShopifyBeta),
+    (r'/b/shopify',                     ButtonsShopifyBeta),
+    (r'/b/shopify/learn',               ButtonsShopifyLearn),
+    (r'/b/shopify/welcome',             ButtonsShopifyWelcome),
+    (r'/b/shopify/upgrade',             ButtonsShopifyUpgrade),
+    (r'/b/shopify/billing_callback',    ButtonsShopifyBillingCallback),
+    (r'/b/shopify/instructions',        ButtonsShopifyInstructions),
+    (r'/b/shopify/error.html',          ButtonsShopifyInstallError),
     (r'/b/shopify/item_shared',         ButtonsShopifyItemShared),
     (r'/b/shopify/start_report',        ButtonsShopifyEmailReports),
     (r'/b/shopify/item_shared_report',  ButtonsShopifyItemSharedReport),
--- apps/buttons/shopify/views.py
+++ apps/buttons/shopify/views.py
@@ -2,8 +2,6 @@
 """ 'Get' functions for the ShopConnection application
 """
 import logging
-import datetime
-from google.appengine.api           import taskqueue
 from google.appengine.ext.webapp    import template
 from apps.buttons.shopify.models    import ButtonsShopify, SharedItem, SharePeriod
 from apps.client.shopify.models     import ClientShopify
@@ -435,72 +433,3 @@ class ButtonsShopifyItemShared(URIHandler):
         self.redirect('%s/static/imgs/noimage.png' % URL)
 
 
-class ButtonsShopifyEmailReports(URIHandler):
-    """Queues the report emails"""
-    def get(self):
-        logging.info("Preparing reports...")
-
-        apps = ButtonsShopify.all().filter(" billing_enabled = ", True)
-
-        for app in apps:
-            logging.info("Setting up taskqueue for %s" % app.client.name)
-            params = {
-                "store": app.store_url,
-            }
-            url = build_url('ButtonsShopifyItemSharedReport')
-            logging.info("taskqueue URL: %s" % url)
-            taskqueue.add(queue_name='buttonsEmail', url=url, params=params)
-
-
-class ButtonsShopifyItemSharedReport(URIHandler):
-    """Sends individual emails"""
-    def get(self):
-        self.post()
-
-    def post(self):
-        product_page = self.request.get('store')
-
-        # We only want the scheme and location to build the url
-        store_url    = "%s://%s" % urlparse(product_page)[:2]
-        app = ButtonsShopify.get_by_url(store_url)
-
-        logging.info("Preparing individual report for %s..." % store_url)
-
-        if app is None:
-            logging.info("App not found!")
-            return
-
-        share_period = SharePeriod.all()\
-                        .filter('app_uuid =', app.uuid)\
-                        .order('-end')\
-                        .get()
-
-        if share_period is None:
-            logging.info("No shares have ever occured")
-            return
-
-        if share_period.end < datetime.date.today():
-            logging.info("No recent period exists -> No shares this period")
-            return
-
-        shares_by_name    = share_period.get_shares_grouped_by_product()
-        shares_by_network = share_period.get_shares_grouped_by_network()
-
-        top_items        = sorted(shares_by_name,
-                                  key=lambda v: v["total_shares"],
-                                  reverse=True)[:10]
-        top_shares       = sorted(shares_by_network,
-                                  key=lambda v: v['shares'],
-                                  reverse=True)
-
-        client = ClientShopify.get_by_url(store_url)
-        if client is None or (client is not None and client.merchant is None):
-            logging.info("No client!")
-            return
-
-        email = client.email
-        shop  = client.name
-        name  = client.merchant.get_full_name()
-
-        Email.report_smart_buttons(email, top_items, top_shares,
-                                 shop_name=shop, client_name=name)
