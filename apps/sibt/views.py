069017b6b6936fd86d3afe4ea17b9e9081aa7679
Brian Lai <b2lai@uwaterloo.ca>
2012-06-06 11:15:33 -0400

    Lower chances of breaking HTTPS

--- apps/sibt/views.py
+++ apps/sibt/views.py
@@ -28,7 +28,7 @@ from apps.sibt.models import SIBT, SIBTInstance, PartialSIBTInstance
 from apps.sibt.shopify.models import SIBTShopify
 from apps.user.models import User
 
-from util.consts import ADMIN_IPS, DOMAIN, P3P_HEADER, PROTOCOL, \
+from util.consts import ADMIN_IPS, DOMAIN, P3P_HEADER, PROTOCOL, SECURE_URL, \
                         SHOPIFY_APPS, UNSURE_DETECTION, URL, USING_DEV_SERVER
 from util.helpers import get_target_url, url
 from util.shopify_helpers import get_shopify_url
@@ -671,6 +671,7 @@ class ColorboxJSServer(URIHandler):
     def get(self):
         template_values = {
             'URL': URL,
+            'DOMAIN': DOMAIN,
             'app_uuid': self.request.get('app_uuid'),
             'user_uuid': self.request.get('user_uuid'),
             'instance_uuid': self.request.get('instance_uuid'),
@@ -1019,7 +1020,7 @@ class SIBTShopifyServeScript(URIHandler):
     """Does everything SIBTServeScript does."""
     def get(self):
         """Does everything SIBTServeScript does."""
-        self.redirect("%s%s?%s" % (URL,
+        self.redirect("%s%s?%s" % (SECURE_URL,
                                    url('SIBTServeScript'),
                                    self.request.query_string),
                       permanent=True)
