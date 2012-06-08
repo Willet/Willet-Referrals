f294c4105f7c042c02a7bfefbe15863743d48468
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2012-06-08 16:25:00 -0400

    [#30268427] Fixed lightswitch
    - Still one last thing: Firefox fix (does not support `background-position-x`)

--- static/js/jquery.lightswitch.js
+++ static/js/jquery.lightswitch.js
@@ -1,85 +1,158 @@
 ﻿/*
  * jQuery LightSwitch plugin
  * @author admin@catchmyfame.com - http://www.catchmyfame.com
- * @version 1.0.1
- * @date December 16, 2010
+ * @author nick@nt3r.com - http://www.nt3r.com
  * @category jQuery plugin
  * @copyright (c) 2010 admin@catchmyfame.com (www.catchmyfame.com)
  * @license CC Attribution-Share Alike 3.0 - http://creativecommons.org/licenses/by-sa/3.0/
  */
 (function($){
-	$.fn.extend({ 
-		lightSwitch: function(options)
-		{
-			var imgPath = "/static/imgs/jquery-lightswitch/";
-			var defaults = 
-			{
-				animSpeed : 120,
-				hoverSpeed : 100,
-				switchImg : imgPath + 'switch.png',
-				switchImgCover: imgPath + 'switchplate.png',
-				switchImgCoverWidth : '63px',
-				switchImgCoverHeight : '18px',
-				disabledImg : imgPath + 'disabled.png',
-				onShift : '0px 0px',
-				offShift : '-37px 0px',
-				peekOff : '-6px 0px',
-				peekOn : '-31px 0px'
-			};
-			var options = $.extend(defaults, options);
-	
-    		return this.each(function() {
-				var o=options;
-				var obj = $(this);
+    $.fn.extend({
+        lightSwitch: function(options) {
+            var defaults = {
+                "hover": {
+                    "speed": 100,
+                    "on":  {"x": "-31px", "y": "0px"},
+                    "off": {"x": "-6px",  "y": "0px"}
+                },
+                "click": {
+                    "speed": 120,
+                    "on":  {"x": "0px",   "y": "0px"},
+                    "off": {"x": "-37px", "y": "0px"}
+                },
+                "dir": "",
+                "switch": "switch.png",
+                "cover": "switchplate.png",
+                "disabled": "disabled.png",
+                "height": "18px",
+                "width": "63px",
+                "change": function(){}
+            };
 
-				if($(this).attr('disabled'))
-				{
-					$(this).css({'display':'none'}).after('<span><img src="'+o.disabledImg+'" /></span>');
-				}
-				else
-				{			
-					$(this).css({'display':'none'}).after('<span class="switch"><img src="'+o.switchImgCover+'" width="'+o.switchImgCoverWidth+'" height="'+o.switchImgCoverHeight+'" /></span>'); //'display':'none'
-				}
-				$(this).next('span.switch').css({'display':'inline-block','background-image':'url("'+o.switchImg+'")','background-repeat':'no-repeat','overflow':'hidden','cursor':'pointer','margin-right':'2px'});
+            options = $.extend(defaults, options);
 
-				$(this).next('span.switch').click(function(){
+            var createLightSwitch = function(disabled) {
+                var o = options,
+                    base = $("<span/>"),
+                    baseImg = $("<img />", {
+                        "src": "" + o.dir + o.disabled
+                    });
 
-					// When we click any span image for a radio button, animate the previously selected radio button to 'off'. 
-					if($(this).prev().is(':radio'))
-					{
-						radioGroupName = $(this).prev().attr('name');
-						$('input[name="'+radioGroupName+'"]'+':checked + span').stop().animate({'background-position':o.offShift},o.animSpeed);
-					}
-					if($(this).prev().is(':checked'))
-					{
-						$(this).stop().animate({'background-position':o.offShift},o.animSpeed); // off
-						$(this).prev().removeAttr('checked');
-					}
-					else
-					{
-						$(this).stop().animate({'background-position':o.onShift},o.animSpeed); // on
-						if($(this).prev().is(':radio')) $('input[name="'+radioGroupName+'"]'+':checked').removeAttr('checked');
-						$(this).prev('input').attr('checked','checked');
-					}
-				}).hover(function(){
-						$(this).stop().animate({'background-position': $(this).prev().is(':checked') ? o.peekOff : o.peekOn},o.hoverSpeed);
-					},function(){
-						$(this).stop().animate({'background-position': $(this).prev().is(':checked') ? o.onShift :o.offShift},o.hoverSpeed);
-				});
-				$(this).next('span.switch').css({'background-position': $(this).is(':checked') ? o.onShift : o.offShift }); // setup default states
+                if (!disabled) {
+                    base.addClass("switch");
 
-				$('input + span').live("click", function() { return false; });
+                    baseImg.attr("width", o.width);
+                    baseImg.attr("height", o.height);
+                    baseImg.attr("src", "" + o.dir + o.cover);
+                }
 
-				$(this).change(function(){
-					radioGroupName = $(this).attr('name');
-					if($(this).is(':radio'))
-					{
-						$(this).stop().animate({'background-position':o.onShift},o.animSpeed);
-						$('input[name="'+radioGroupName+'"]'+' + span').stop().animate({'background-position':o.offShift},o.animSpeed);
-					}
-					$(this).next('span').stop().animate({'background-position': $(this).is(':checked') ? o.onShift :o.offShift},o.animSpeed);
-				});
-  			});
-    	}
-	});
+                base.append(baseImg);
+                base.css({
+                    'display':'inline-block',
+                    'background-image':'url("' + o.dir + o["switch"] + '")',
+                    'background-repeat':'no-repeat',
+                    'overflow':'hidden',
+                    'cursor':'pointer',
+                    'margin-right':'2px'
+                });
+
+                return base;
+            };
+
+            return this.each(function() {
+                var o=options,
+                    lightSwitch,
+                    input = $(this);
+
+                // Create lightswitch
+                lightSwitch = createLightSwitch(input.attr('disabled'), o);
+
+                // Replace input with lightswitch
+                input.hide().after(lightSwitch);
+
+                // Setup switch handlers
+                lightSwitch.click(function() {
+                    var c = o.click,
+                        radioGroupName;
+
+                    // When we click any span image for a radio button, animate the previously selected radio button to 'off'.
+                    if(input.is(':radio')) {
+                        radioGroupName = $(this).prev().attr('name');
+                        $('input[name="'+radioGroupName+'"]'+':checked + span').stop().animate({
+                            'background-position-x': c.off.x,
+                            'background-position-y': c.off.y
+                        }, c.speed);
+                    }
+
+                    if(input.is(':checked')) {
+                        $(this).stop().animate({
+                            'background-position-x': c.off.x,
+                            'background-position-y': c.off.y
+                        }, c.speed); // off
+                        input.removeAttr('checked');
+                    } else {
+                        $(this).stop().animate({
+                            'background-position-x': c.on.x,
+                            'background-position-y': c.on.y
+                        }, c.speed); // on
+
+                        if(input.is(':radio')){
+                            $('input[name="'+radioGroupName+'"]'+':checked').removeAttr('checked');
+                        }
+                        input.attr('checked','checked');
+                    }
+
+                    //because change is not triggered unless user clicks a checkbox
+                    input.change();
+
+                }).hover(
+                    function() {
+                        var h = o.hover;
+                        $(this).stop().animate({
+                            'background-position-x': input.is(':checked') ? h.off.x : h.on.x,
+                            'background-position-y': input.is(':checked') ? h.off.y : h.on.y
+                        }, h.speed);
+                    },
+                    function(){
+                        var c = o.click;
+                        $(this).stop().animate({
+                            'background-position-x': input.is(':checked') ? c.on.x : c.off.x,
+                            'background-position-y': input.is(':checked') ? c.on.y : c.off.y
+                        }, c.speed);
+                    }
+                );
+
+                lightSwitch.css({
+                    'background-position-x': $(this).is(':checked') ? o.click.on.x : o.click.off.x,
+                    'background-position-y': $(this).is(':checked') ? o.click.on.y : o.click.off.y
+                }); // setup default states
+
+                $('input + span').live("click", function() { return false; });
+
+                input.change(function(event){
+                    var c = o.click,
+                        radioGroupName = $(this).attr('name');
+
+                    if($(this).is(':radio')) {
+                        $(this).animate({
+                            'background-position-x': c.on.x,
+                            'background-position-y': c.on.y
+                        }, c.speed);
+
+                        $('input[name="'+radioGroupName+'"]'+' + span').stop().animate({
+                            'background-position-x': c.off.x,
+                            'background-position-y': c.off.x
+                        }, c.speed);
+                    }
+
+                    lightSwitch.animate({
+                        'background-position-x': $(this).is(':checked') ? c.on.x : c.off.x,
+                        'background-position-y': $(this).is(':checked') ? c.on.y : c.off.y
+                    }, c.speed);
+
+                    o.change(event);
+                });
+            });
+        }
+    });
 })(jQuery);
