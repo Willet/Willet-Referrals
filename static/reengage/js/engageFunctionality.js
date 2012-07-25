8e449e1c23c4576ff49d81d244f172f3d4f5ae26
Brian Lai <b2lai@uwaterloo.ca>
2012-07-25 17:18:40 -0400

    fix post-create/delete selection issues

--- static/reengage/js/engageFunctionality.js
+++ static/reengage/js/engageFunctionality.js
@@ -130,8 +130,8 @@ var removePost = function (uuid) {
         "Confirm",
         "Are you sure you want to delete this post?",
         function () {
-            $('#' + uuid).remove();
             deletePost(uuid);
+            $('#' + uuid).remove();
             // updateQueueUI();
         }
     );
@@ -398,6 +398,7 @@ $(document).ready(function () {
     //Delete post
     $(document).on("click", ".postDelete", function () {
         var uuid = $(this).parent().attr("id");
+        console.log(uuid);
         removePost(uuid);
     });
 
