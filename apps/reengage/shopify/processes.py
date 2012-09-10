5031036bf553080ac07f6a77cba38fa047de9bca
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2012-09-10 17:25:09 -0400

    More performance enhancements
    - We can now pre-load the queues...
        - However, _get_products is now a bottleneck, and I'm a bit stumped how to overcome this.

--- apps/reengage/shopify/processes.py
+++ apps/reengage/shopify/processes.py
@@ -1,7 +1,12 @@
 import logging
+from google.appengine.api import taskqueue
+from apps.client.models import Client
+from apps.product.models import Product
+from apps.product.shopify.models import ProductShopifyCollection
 from apps.product.shopify.processes import \
     create_product, update_product, delete_product, \
     create_collection, update_collection, delete_collection
+from util.helpers import url
 from util.urihandler import URIHandler
 
 #TODO: We should just get_or_create the product instead of duplicating
@@ -48,3 +53,65 @@ class DeleteReEngageCollectionsShopify(URIHandler):
     """Delete a Shopify collection"""
     def post(self):
         delete_collection(self.request)
+
+
+class GetOrCreateShopifyQueues(URIHandler):
+    """Creates missing queues"""
+    def get(self):
+        self.post()
+
+    def post(self):
+        """Creates missing queues.
+
+        get_or_create_queue is expensive. So, do it with taskqueues"""
+
+        client_uuid = self.request.get("client_uuid")
+
+        client = Client.get(client_uuid)
+
+        if not client:
+            raise ValueError("No client found for client_uuid: %s" % client_uuid)
+
+        for collection in client.collections:
+            taskqueue.add(url=url("GetOrCreateShopifyQueue"), params={
+                "uuid": collection.uuid,
+                "type": "Collection"
+            })
+
+        for product in client.products:
+            taskqueue.add(url=url("GetOrCreateShopifyQueue"), params={
+                "uuid": product.uuid,
+                "type": "Product"
+            })
+
+        self.response.out.write("OK")
+
+
+class GetOrCreateShopifyQueue(URIHandler):
+    """Creates missing queues"""
+    def get(self):
+        self.post()
+
+    def post(self):
+        """Creates missing queues.
+
+        get_or_create_queue is expensive. So, do it with taskqueues"""
+        uuid = self.request.get("uuid")
+        type = self.request.get("type")
+
+        if type == "Collection":
+            object = ProductShopifyCollection.get(uuid)
+        elif type == "Product":
+            object = Product.get(uuid)
+        else:
+            logging.error("Incompatible type: %s" % type)
+            return
+
+        if not object:
+            logging.error("No %s found for col_uuid: %s" % (type, uuid))
+            return
+
+        # Implicitly does _get_or_create_queue()
+        queue = object.queue
+
+        self.response.out.write("OK")
