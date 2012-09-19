739bdea3687182bab42a557f98cd0745e1f83533
Grigory Kruglov <grigory.kruglov@gmail.com>
2012-09-19 15:02:02 -0400

    Add support for "add to cart" tracking; refactor tracking.js

--- templates/analytics/leadspeaker_shopify.js
+++ templates/analytics/leadspeaker_shopify.js
@@ -1,6 +1,7 @@
-(function(window) {
+var WILLET_TRACKING = (function($, window, document) {
+    var category = "";
+
 	var trackEvent = function (category, action, value) {
-        console.log("trackEvent: ", category, action, value);
 	    var iframe = document.createElement( 'iframe' );
 	    
 	    // category and action are required.
@@ -31,10 +32,8 @@
 
     var fromLeadSpeaker = function() {
     	if (window.location.pathname.indexOf("leadspeaker_cohort_id") != -1) {
-            console.log("from leadspeaker");
 			return true;
 		}
-        console.log("not from leadspeaker");
 		return false;
     }
 
@@ -43,34 +42,72 @@
     		"https://www.facebook.com/": "facebook",
     		"http://www.facebook.com/": "facebook"}
 
-        console.log("from network: ", referrers[document.referrer]);
     	return referrers[document.referrer];
     }
 
     var getCohortId = function() {
     	if (!fromLeadSpeaker()) {
-            console.log("no cohort id");
     		return false;
     	}
     	cohort_id = window.location.href.split("leadspeaker_cohort_id=")[1];
-    	console.log("cohort id", cohort_id);
         return cohort_id;
     }
 
-    var category = "";
-    var action = "";
-    var cohort_id = getCohortId() || "";
+    var CartTracking = (function() {
+        var cohort_id = getCohortId() || "",
+            action = "add_to_cart";
 
-    if (fromLeadSpeaker()) {
-    	category = "leadspeaker";
-    	action = "open_product";
-    } else {
-    	category = "unknown";
+        // set listener for clicks on "add to cart"
+        var init = function() {
+            // switch away from using page-supplied jquery....
+            $("#add-to-cart").click(function() {
+                WILLET_TRACKING.cart.add();
+            })
+        }
+
+        var trackAdd = function() {
+            trackEvent(category, action, cohort_id);
+        }
+
+        return {
+            "init": init,
+            "add": trackAdd
+        }
+    }());
+
+    var ProductTracking = (function() {
+        var cohort_id = getCohortId() || "",
+            action = "open_product";
+
+        var init = function() {
+            trackEvent(category, action, cohort_id);
+        }
+
+        return {
+            "init": init
+        }
+    }());
+
+    var init = function() {
+        if (fromLeadSpeaker()) {
+            category = "leadspeaker";
+        } else {
+            category = "unknown";
+        }
+
+        if (fromWhichNetwork() == "facebook") {
+            category += "_facebook";
+        }
+
+        // @TODO: invoke these conditionally if on product page or on cart page
+        ProductTracking.init();
+        CartTracking.init();
     }
 
-    if (fromWhichNetwork() == "facebook") {
-    	category += "_facebook";
+    return {
+        "init": init,
+        "cart": CartTracking
     }
+})($, window, document);
 
-    trackEvent(category, action, cohort_id);
-})(window);
+WILLET_TRACKING.init();
