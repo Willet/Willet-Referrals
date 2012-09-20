a388fbeaaf81b8681a54a941b598cd29fe1a7681
Grigory Kruglov <grigory.kruglov@gmail.com>
2012-09-20 14:04:39 -0400

    Make js lint happy. Very happy. Perhaps too happy.

--- templates/analytics/leadspeaker_shopify.js
+++ templates/analytics/leadspeaker_shopify.js
@@ -1,223 +1,225 @@
-var WILLET_TRACKING = (function($, window, document) {
-    var category = "", landing_site = "{{ landing_site }}";
+var willetTracking = (function ($, window, document) {
+    "use strict";
 
-	var trackEvent = function (category, action, value) {
-	    var iframe = document.createElement( 'iframe' );
-	    
-	    // category and action are required.
-	    if (!category || !action) {
-	    	return;
-	    }
+    var category = "",
+        landing_site = "{{ landing_site }}",
 
-	    value = value || "";
+        trackEvent = function (category, action, value) {
+            var iframe = document.createElement('iframe');
 
-	    iframe.style.display = 'none';
-        iframe.src = "{{ SECURE_URL }}/an/trackEvent?" +
-                     "&category=" + category +
-                     "&action=" + action +
-                     "&value=" + value +
-                     "&hostname=" + "{{ shop_hostname }}" || window.location.hostname +
-                     "&pathname=" + window.location.pathname;
-
-        {% if client %}
-            iframe.src += "&client=" + "{{ client.uuid }}";
-        {% endif %}
-
-        {% if user %}
-            iframe.src += "&user=" + "{{ user.uuid }}";
-        {% endif %}
-
-        document.body.appendChild( iframe );
-    };
-
-    var onCartPage = function() {
-        if (window.location.pathname == "/cart") {
-            return true;
-        }
-        return false;
-    }
-
-    var onThankYouPage = function() {
-        if (window.location.hostname == "checkout.shopify.com") {
-            return true;
-        }
-        return false;
-    }
-
-    var fromLeadSpeaker = function() {
-    	if (window.location.pathname.indexOf("leadspeaker_cohort_id") != -1) {
-			return true;
-		}
-		return false;
-    }
-
-    var fromWhichNetwork = function() {
-    	var referrers = {
-    		"https://www.facebook.com/": "facebook",
-    		"http://www.facebook.com/": "facebook"}
-
-    	return referrers[document.referrer];
-    }
-
-    var getCohortId = function() {
-    	if (!fromLeadSpeaker()) {
-    		return false;
-    	}
-    	cohort_id = window.location.href.split("leadspeaker_cohort_id=")[1];
-        return cohort_id;
-    }
-
-    var CartButtonTracking = (function() {
-        var cohort_id = getCohortId() || "",
-            action = "add_to_cart";
-
-        // set listener for clicks on "add to cart"
-        var init = function() {
-            // switch away from using page-supplied jquery....
-            $("#add-to-cart").click(function() {
-                WILLET_TRACKING.cart_button.click();
-            })
-        }
-
-        var trackAdd = function() {
-            trackEvent(category, action, cohort_id);
-        }
-
-        return {
-            "init": init,
-            "click": trackAdd
-        }
-    }());
-
-    var ProductTracking = (function() {
-        var cohort_id = getCohortId() || "",
-            action = "open_product";
-
-        var init = function() {
-            trackEvent(category, action, cohort_id);
-        }
-
-        return {
-            "init": init
-        }
-    }());
-
-    var CartPageTracking = (function() {
-        var init = function() {
-            var leadspeaker_category = Cookies.read("leadspeaker_category") || "unknown",
-                leadspeaker_cid = Cookies.read("leadspeaker_cid") || "",
-                action = "open_cart";
-
-            trackEvent(leadspeaker_category, action, leadspeaker_cid);
-        }
-
-        return {
-            "init": init
-        }
-    }());
-
-    var ThankYouTracking = (function() {
-        var init = function() {
-            var action = "thankyou_page";
-
-            // in the instance of tracking.js invoked through script_tags
-            // and not through additional script added through store admin
-            if (!landing_site) {
+            // category and action are required.
+            if (!category || !action) {
                 return;
             }
 
-            // this assumes that landing_site will end, if it's there, in our cohort_id
-            split_url = landing_site.split("leadspeaker_cohort_id=")
-            
-            // not from us
-            if (split_url.length == 1) {
-                leadspeaker_category = "unkown";
-                leadspeaker_cid = "";
+            value = value || "";
 
-            // from us, but not sure which social network we're coming from
+            iframe.style.display = 'none';
+            iframe.src = "{{ SECURE_URL }}{% url TrackEvent %}?" +
+                         "category=" + category +
+                         "&action=" + action +
+                         "&value=" + value +
+                         "&pathname=" + window.location.pathname +
+                         "&hostname=" + "{{ shop_hostname }}" || window.location.hostname;
+
+            document.body.appendChild(iframe);
+        },
+
+        onCartPage = function () {
+            if (window.location.pathname === "/cart") {
+                return true;
+            }
+            return false;
+        },
+
+        onThankYouPage = function () {
+            if (window.location.hostname === "checkout.shopify.com") {
+                return true;
+            }
+            return false;
+        },
+
+        fromLeadSpeaker = function () {
+            if (window.location.pathname.indexOf("leadspeaker_cohort_id") !== -1) {
+                return true;
+            }
+            return false;
+        },
+
+        fromWhichNetwork = function () {
+            var referrers = {
+                "https://www.facebook.com/": "facebook",
+                "http://www.facebook.com/": "facebook"
+            };
+
+            return referrers[document.referrer];
+        },
+
+        getCohortId = function () {
+            if (!fromLeadSpeaker()) {
+                return false;
+            }
+            var cohort_id = window.location.href.split("leadspeaker_cohort_id=")[1];
+            return cohort_id;
+        },
+
+        CartButtonTracking = (function () {
+            var cohort_id = getCohortId() || "",
+                action = "add_to_cart",
+
+                // set listener for clicks on "add to cart"
+                init = function () {
+                    // switch away from using page-supplied jquery....
+                    $("#add-to-cart").click(function () {
+                        willetTracking.cart_button.click();
+                    });
+                },
+
+                trackAdd = function () {
+                    trackEvent(category, action, cohort_id);
+                };
+
+            return {
+                "init": init,
+                "click": trackAdd
+            };
+        }()),
+
+        ProductTracking = (function () {
+            var cohort_id = getCohortId() || "",
+                action = "open_product",
+
+                init = function () {
+                    trackEvent(category, action, cohort_id);
+                };
+
+            return {
+                "init": init
+            };
+        }()),
+
+        CartPageTracking = (function () {
+            var init = function () {
+                var leadspeaker_category = willetTracking.cookies.read("leadspeaker_category") || "unknown",
+                    leadspeaker_cid = willetTracking.cookies.read("leadspeaker_cid") || "",
+                    action = "open_cart";
+
+                trackEvent(leadspeaker_category, action, leadspeaker_cid);
+            };
+
+            return {
+                "init": init
+            };
+        }()),
+
+        ThankYouTracking = (function () {
+            var init = function () {
+                var action = "thankyou_page",
+                    // this assumes that landing_site will end, if it's there, in our cohort_id
+                    split_url = landing_site.split("leadspeaker_cohort_id="),
+                    leadspeaker_category = "unkown",
+                    leadspeaker_cid = "";
+
+                // in the instance of tracking.js invoked through script_tags
+                // and not through additional script added through store admin
+                if (!landing_site) {
+                    return;
+                }
+
+                // from us, but not sure which social network we're coming from
+                if (split_url.length !== 1) {
+                    leadspeaker_category = "leadspeaker";
+                    leadspeaker_cid = split_url[1];
+                }
+
+                trackEvent(leadspeaker_category, action, leadspeaker_cid);
+            };
+
+            return {
+                "init": init
+            };
+        }()),
+
+        Cookies = (function () {
+            // Generic cookie library
+            // Source: http://www.quirksmode.org/js/cookies.html
+            var create = function (name, value, days) {
+                    var expires = "",
+                        date = new Date();
+
+                    if (days) {
+                        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+                        expires = "; expires=" + date.toGMTString();
+                    }
+                    document.cookie = name + "=" + value + expires + "; path=/";
+                },
+
+                read = function (name) {
+                    var nameEQ = name + "=",
+                        ca = document.cookie.split(';'),
+                        c,
+                        i;
+
+                    for (i = 0; i < ca.length; i++) {
+                        c = ca[i];
+                        while (c.charAt(0) === ' ') {
+                            c = c.substring(1, c.length);
+                        }
+                        if (c.indexOf(nameEQ) === 0) {
+                            return c.substring(nameEQ.length, c.length);
+                        }
+                    }
+                    return null;
+                },
+
+                erase = function (name) {
+                    create(name, "", -1);
+                };
+
+            return {
+                "create": create,
+                "read": read,
+                "erase": erase
+            };
+        }()),
+
+        init = function () {
+            var cohort_id = getCohortId() || "";
+
+            if (fromLeadSpeaker()) {
+                category = "leadspeaker";
             } else {
-                leadspeaker_category = "leadspeaker";
-                leadspeaker_cid = split_url[1];
+                category = "unknown";
             }
 
-            trackEvent(leadspeaker_category, action, leadspeaker_cid);
-        }
-
-        return {
-            "init": init
-        }
-    }());
-
-    var Cookies = (function() {
-        // Generic cookie library
-        // Source: http://www.quirksmode.org/js/cookies.html
-        var create = function (name, value, days) {
-            if (days) {
-                var date = new Date();
-                date.setTime(date.getTime()+(days*24*60*60*1000));
-                var expires = "; expires="+date.toGMTString();
-            } else var expires = "";
-            document.cookie = name+"="+value+expires+"; path=/";
-        }
-
-        var read = function (name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for(var i=0;i < ca.length;i++) {
-                var c = ca[i];
-                while (c.charAt(0)==' ') c = c.substring(1,c.length);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-            }
-            return null;
-        }
-
-        var erase = function (name) {
-            create(name,"",-1);
-        }
-
-        return {
-            "create": create,
-            "read": read,
-            "erase": erase
-        }
-    }());
-
-    var init = function() {
-        var cohort_id = getCohortId() || "";
-
-        if (fromLeadSpeaker()) {
-            category = "leadspeaker";
-        } else {
-            category = "unknown";
-        }
-
-        if (fromWhichNetwork() == "facebook") {
-            category += "_facebook";
-        }
-
-        if (onCartPage()) {
-            CartPageTracking.init();
-
-        } else if (onThankYouPage()) {
-            ThankYouTracking.init();
-
-        // on product page
-        } else {
-            // set tracking cookies
-            Cookies.create("leadspeaker_category", category, 7);
-            if (cohort_id) {
-                Cookies.create("leadspeaker_cid", cohort_id, 7);
+            if (fromWhichNetwork() === "facebook") {
+                category += "_facebook";
             }
 
-            ProductTracking.init();
-            CartButtonTracking.init();
-        }
-    }
+            if (onCartPage()) {
+                CartPageTracking.init();
+
+            } else if (onThankYouPage()) {
+                ThankYouTracking.init();
+
+            // on product page
+            } else {
+                // set tracking cookies
+                willetTracking.cookies.create("leadspeaker_category", category, 7);
+                if (cohort_id) {
+                    willetTracking.cookies.create("leadspeaker_cid", cohort_id, 7);
+                }
+
+                ProductTracking.init();
+                CartButtonTracking.init();
+            }
+        };
 
     return {
         "init": init,
-        "cart_button": CartButtonTracking
-    }
-})($, window, document);
+        "cart_button": CartButtonTracking,
+        "cookies": Cookies
+    };
+}($, window, document));
 
-WILLET_TRACKING.init();
+willetTracking.init();
